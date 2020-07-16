c0602c478283be7a42610c553c0dc0ec33eaafc8
Wolfgang Pfnür <wolfgang.pfnuer@weq.com>
2020-07-16 09:31:04 +0200

    hammerspoon

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,13 +1,14 @@
 -- constants
--- local applications = {"Google Chrome", "Mail", "Calendar", "Skype", "iTerm"}
--- local laptopScreen = "Color LCD"
--- local windowLayout = {
---     {"Google Chrome",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
---     {"Mail",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
---     {"Calendar",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
---     {"Skype",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
---     {"iTerm",   nil,          laptopScreen, hs.layout.maximized, nil, nil},
--- }
+local applications = {"Google Chrome", "Mail", "Calendar", "Slack", "iTerm2"}
+local laptopScreen = "Color LCD"
+local monitor = "C27F591"
+local windowLayout = {
+    {"Google Chrome",  nil,          laptopScreen, hs.layout.maximized,    nil, nil},
+    {"Mail",  nil,          laptopScreen, hs.layout.maximized,    nil, nil},
+    {"Calendar",  nil,          laptopScreen, hs.layout.maximized,    nil, nil},
+    {"Slack",    nil,          laptopScreen, hs.layout.maximized,   nil, nil},
+    {"iTerm2",   nil,          monitor, hs.layout.maximized, nil, nil},
+}
 
 
 switchableHotkeys = {}
@@ -48,28 +49,28 @@ end))
 
 
 -- PUT ALL WINDOWS WHERE THEY BELONG
--- function applyLayout()
---     hs.layout.apply(windowLayout)
---     for _, window in pairs(hs.window.allWindows()) do
---         if (window:application():title() == "iTerm") then
---             if not window:isFullScreen() then
---                 window:toggleFullScreen()
---             end
---         end
---     end
+function applyLayout()
+    for _, window in pairs(hs.window.allWindows()) do
+        if (window:application():title() == "iTerm2") then
+            if window:isFullScreen() then
+                window:toggleFullScreen()
+            end
+        end
+    end
+    os.execute("sleep 1")
+    hs.layout.apply(windowLayout)
+    os.execute("sleep 1")
+    for _, window in pairs(hs.window.allWindows()) do
+        if (window:application():title() == "iTerm2") then
+            if not window:isFullScreen() then
+                window:toggleFullScreen()
+            end
+        end
+    end
 
---     -- stop powerline-daemon regularly
---     os.execute('pgrep -f powerline-daemon | xargs kill')
-
---     -- restart nexTab
---     for _, application in pairs(hs.application.runningApplications()) do
---         if (application:title() == 'nexTab') then
---             application:kill()
---             os.execute('sleep 1')
---         end
---     end
---     hs.application.launchOrFocus('nexTab')
--- end
+    -- -- stop powerline-daemon regularly
+    -- os.execute('pgrep -f powerline-daemon | xargs kill')
+end
 
 
 
@@ -106,59 +107,45 @@ end))
 
 
 -- PUT ALL WINDOWS WHERE THEY BELONG WHEN SCREENS CHANGE
--- function screenWatcherFun()
---     applyLayout()
+function screenWatcherFun()
+    applyLayout()
+end
+
+local screenWatcher = hs.screen.watcher.new(screenWatcherFun)
+screenWatcher:start()
+
+
+
+
+-- -- AUTOTEST BINGO ON CHANGES
+-- for _, service in pairs({"recommender", "librarian", "academy", "common"}) do
+--     hs.pathwatcher.new(os.getenv("HOME") .. "/tron/tron/" .. service, function(files)
+--         for _, file in pairs(files) do
+--             if string.find(file, '.py') and not string.find(file, '.pyc') then
+--                 io.popen(os.getenv("SHELL") .. " -l -i -c 'tmux send -t 2 \"C-c\" && tmux send -Rt 2 \".venv/bin/pytest --cov-fail-under=0 -n auto tron -k " .. service .. "\" enter'", 'r')
+--                 return
+--             end
+--         end
+--     end):start()
+--     hs.pathwatcher.new(os.getenv("HOME") .. "/tron/tron/test/" .. service, function(files)
+--         for _, file in pairs(files) do
+--             if string.find(file, '.py') and not string.find(file, '.pyc') then
+--                 io.popen(os.getenv("SHELL") .. " -l -i -c 'tmux send -t 2 \"C-c\" && tmux send -Rt 2 \".venv/bin/pytest --cov-fail-under=0 -n auto tron -k " .. service .. "\" enter'", 'r')
+--                 return
+--             end
+--         end
+--     end):start()
 -- end
 
--- local screenWatcher = hs.screen.watcher.new(screenWatcherFun)
--- screenWatcher:start()
 
-
-
-
--- AUTOTEST BINGO ON CHANGES
-for _, service in pairs({"recommender", "librarian", "academy", "common"}) do
-    hs.pathwatcher.new(os.getenv("HOME") .. "/tron/tron/" .. service, function(files)
-        for _, file in pairs(files) do
-            if string.find(file, '.py') and not string.find(file, '.pyc') then
-                io.popen(os.getenv("SHELL") .. " -l -i -c 'tmux send -t 2 \"C-c\" && tmux send -Rt 2 \".venv/bin/pytest --cov-fail-under=0 -n auto tron -k " .. service .. "\" enter'", 'r')
-                return
-            end
-        end
-    end):start()
-    hs.pathwatcher.new(os.getenv("HOME") .. "/tron/tron/test/" .. service, function(files)
-        for _, file in pairs(files) do
-            if string.find(file, '.py') and not string.find(file, '.pyc') then
-                io.popen(os.getenv("SHELL") .. " -l -i -c 'tmux send -t 2 \"C-c\" && tmux send -Rt 2 \".venv/bin/pytest --cov-fail-under=0 -n auto tron -k " .. service .. "\" enter'", 'r')
-                return
-            end
-        end
-    end):start()
-end
-
-
-
--- AUTORELOAD CONFIG
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "dotfiles/dotfiles/hammerspoon/", hs.reload):start()
 
 
 -- HOTKEY REPOSITION WINDOWS
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
   applyLayout()
 end)
-
-
 
 
 
