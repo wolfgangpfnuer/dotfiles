5ef6f8a0e1cc6eab6385facc7037c2f76fea8207
Wolfgang Pfnür <wolfgang.pfnuer@swaarm.com>
2022-08-10 11:16:48 +0200

    swift, ulimit

--- dotfiles/config/lvim/config.lua
+++ dotfiles/config/lvim/config.lua
@@ -117,6 +117,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
+  { command = "swiftformat", filetypes = { "swift" } },
   --   {
   --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   --     command = "prettier",
